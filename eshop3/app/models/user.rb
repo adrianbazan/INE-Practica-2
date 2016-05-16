5a246d9e7f52a2d6144201826f1a666e10ff18f8
Francisco Javier P.R <franciscojavier.poncerosales@gmail.com>
2016-05-16 17:18:29 +0200

    Solucionado algunos errores del modelo, pero quedan algunos que no se donde estan definidos

--- eshop3/app/models/user.rb
+++ eshop3/app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
+  HUMANIZED_ATTRIBUTES = {
+    :name => "Nombre",
+    :login => "Usuario",
+    :email => "E-mail",
+    :password => "Contraseña",
+    :password_confirmation => "Confirmación de contraseña",
+  }
+  
+  def self.human_attribute_name(attr, options = {})
+    HUMANIZED_ATTRIBUTES[attr.to_sym] || super
+  end
+
   acts_as_authentic do |a|
     a.validate_login_field = true
     a.validate_password_field = true
@@ -8,7 +20,12 @@ class User < ActiveRecord::Base
     a.crypto_provider = Authlogic::CryptoProviders::Sha512
   end
 
-  validates_presence_of :name, :login, :email, :password, :password_confirmation
-  validates_length_of :name, :in => 3..225
-  validates_uniqueness_of :name, :login, :email
+  validates_presence_of :name, message:"no puede estar vacío"
+  validates_presence_of :login, message:"no puede estar vacío"
+  validates_presence_of :email, message:"no puede estar vacío"
+  validates_presence_of :password, message:"no puede estar vacío"
+  validates_presence_of :password_confirmation, message:"no puede estar vacío"
+  validates_length_of :name, :in => 3..225, message:"no puede estar vacío"
+  validates_uniqueness_of :name, :login, :email, message:"debe ser único"
+
 end
